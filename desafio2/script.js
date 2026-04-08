0757e19d904b11e03dddaf79a772a329804bcca4
Eduardo Moraes <eduzinho2118@gmail.com>
2026-04-07 22:29:51 -0300

    html e js feitos

--- desafio2/script.js
+++ desafio2/script.js
@@ -1,1 +1,15 @@
-console.log('ola mundo');
+function formularioLogin(){
+    const user = document.getElementById('user');
+    const password = document.getElementById('senha')
+    const btnValidar = document.getElementById('validar')
+
+    btnValidar.addEventListener('click', ()=>{
+        let usuario = user.value;
+        let senha = password.value;
+
+        if(isNaN(senha)){
+            alert('verificar senha!')
+            return
+        }
+    })
+}
